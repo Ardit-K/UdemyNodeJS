4679ccc56567b210b84eca229859172384b4f706
ardit <arditkoti7192@gmail.com>
2024-07-04 10:36:26 -0400

    Routing and redirect basics.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 // function rqListener(req, res){
 
 // }
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
