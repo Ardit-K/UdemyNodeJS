f8cc091206c33b2ad10a1d564938da025c19f450
ardit <arditkoti7192@gmail.com>
2024-07-04 10:48:02 -0400

    Parsing requests

--- server/app.js
+++ server/app.js
@@ -16,7 +16,16 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if(url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'DUMMMY');
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
