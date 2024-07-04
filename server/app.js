fbc598082d33503a5958c0139d1ead2fce4c2c5e
ardit <arditkoti7192@gmail.com>
2024-07-04 11:00:46 -0400

    Blocking request code.

--- server/app.js
+++ server/app.js
@@ -21,14 +21,15 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', ()=>{
+        return req.on('end', ()=>{
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+            fs.writeFile('message.txt', message, err=>{
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });  
     }
 
     res.setHeader('Content-Type', 'text/html');
